ef247fcb447164ef8c80d83100eca1b15e2b3ef8
Eric Hayes <eric@erichayes.net>
2014-11-18 21:04:18 -0800

    Move (max 50) check to end of method

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -67,18 +67,16 @@ class ItemUpdater
   end
   
   def update_backstage_pass_before_expiration
-    if item.quality < 50
+    item.quality += 1
+    if item.sell_in < 11
       item.quality += 1
-      if item.sell_in < 11
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-      if item.sell_in < 6
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
+    end
+    if item.sell_in < 6
+      item.quality += 1
+    end
+
+    if item.quality > 50
+      item.quality = 50
     end
   end
   
